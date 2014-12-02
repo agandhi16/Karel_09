acefc5d3666013e0049a96165b13f6e73bb054b0
agandhi16 <alex_gandhi@asl.org>
2014-12-02 10:56:39 +0000

    Final Code

--- Lab09.java
+++ Lab09.java
@@ -7,7 +7,8 @@
  *
  */
 
-import edu.fcps.karel2.Display; 
+import edu.fcps.karel2.Display;
+import edu.fcps.karel2.Robot;  
 import javax.swing.JOptionPane;
 
 public class Lab09 {
@@ -16,8 +17,30 @@ public class Lab09 {
 		 String map = JOptionPane.showInputDialog("Which map?");
 		 Display.openWorld("maps/"+map+".map");
 		 Display.setSize(10, 10);
-		 Athlete athena = new Athlete(1, 1, Display.EAST, 0);
-		 //TODO write a combination of definite and indefinite loops that will cause athena
-		 // to pick up each pile of beepers and deposit them one square to the right.
-	 }
- }
+       Display.setSpeed(5);
+		 Athlete alex = new Athlete(1, 1, Display.EAST, 0);
+        
+       alex.move();
+       alex.move();
+       alex.move();
+       alex.move();
+       alex.move();
+       alex.move();
+       
+       for (int k = 1; k <= 7; k++) {
+       int count = 0;
+       while (alex.nextToABeeper()){
+       alex.pickBeeper();
+       count++;}
+       alex.move();
+       for (int g = 1; g <= count; count--){
+       alex.putBeeper();}
+       alex.turnAround();
+       if (alex.frontIsClear()) {
+       alex.move();}
+       if (alex.frontIsClear()) {
+       alex.move();}
+       alex.turnAround();}
+       alex.turnAround();   
+  }
+}
